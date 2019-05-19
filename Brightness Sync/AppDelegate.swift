3e716e781541b56099b0481946a6bede399242c2
Onne van Dijk <onnevandijk@icloud.com>
2019-05-19 17:47:41 +0200

    Initial Commit

---
+++ Brightness Sync/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Brightness Sync
+//
+//  Created by Onne van Dijk on 19/05/2019.
+//  Copyright © 2019 Onne van Dijk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
