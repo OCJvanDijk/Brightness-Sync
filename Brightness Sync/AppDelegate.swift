e943d7e7e1d12047fe20908cfa13e4c6dd394844
Onne van Dijk <onnevandijk@icloud.com>
2019-06-07 13:25:06 +0200

    Removed default top comments

--- Brightness Sync/AppDelegate.swift
+++ Brightness Sync/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Brightness Sync
-//
-//  Created by Onne van Dijk on 19/05/2019.
-//  Copyright © 2019 Onne van Dijk. All rights reserved.
-//
-
 import Cocoa
 import os
 
--- Brightness Sync/main.swift
+++ Brightness Sync/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  Brightness Sync
-//
-//  Created by Onne van Dijk on 19/05/2019.
-//  Copyright © 2019 Onne van Dijk. All rights reserved.
-//
-
 import Cocoa
 
 let app = NSApplication.shared
