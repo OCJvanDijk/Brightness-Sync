f6f251335fb143bdf9f29d7f6dd743e7177723ce
Onne van Dijk <onnevandijk@icloud.com>
2020-03-31 19:02:04 +0200

    Launch app from main.swift

--- BrightnessSyncLauncher/main.swift
+++ BrightnessSyncLauncher/main.swift
@@ -1,7 +1,6 @@
 import Cocoa
 
-let app = NSApplication.shared
-let delegate = AppDelegate()
-app.delegate = delegate
+let launcherURL = Bundle.main.bundleURL
+let appURL = launcherURL.appendingPathComponent("../../../..").standardized
 
-app.run()
+NSWorkspace.shared.launchApplication(appURL.path)
